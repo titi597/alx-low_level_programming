a166df1f3a04f40930960f77946362745af108f8
titi597 <139129034+titi597@users.noreply.github.com>
2023-08-10 05:02:43 -0700

    committed size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long long int y;
 	float z;
 
-printf("size of a char: %zu byte(s)\n", (unsigned long)sizeof(v));
-printf("size of an int: %zu byte(s)\n", (unsigned long)sizeof(w));
-printf("size of a long int: %zu byte(s)\n", (unsigned long)sizeof(x));
-printf("size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(y));
-printf("size of a float: %zu byte(s)\n", (unsigned long)sizeof(z));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(v));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(w));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
 return (0);
 }
