e65997193dc02b8a7ccabb318a2100d61f3580f5
titi597 <139129034+titi597@users.noreply.github.com>
2023-08-15 03:02:47 -0700

    committed add x+y

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - addition of two integers
+ *
+ * Description: add two integers
+ * @x: first integer
+ * @y: second integer
+ *
+ * Return: 0 (success)
+ */
+
+int add(int x, int y)
+{
+	int result = x + y;
+
+	return (result);
+}
