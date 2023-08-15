e1cdce38d940598fdb89db0a72bbbc020f9d9f4a
titi597 <139129034+titi597@users.noreply.github.com>
2023-08-15 06:19:38 -0700

    Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Enrty point for printing multiple of 3 or 5
+ *
+ * Description: computing the sum of all the multiples of 3 or 5 
+ * below 1024 (excluded)
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (y = 0; y < 1024; y++)
+	{
+		if ((y % 3 == 0) || (y % 5 == 0))
+			x += y;
+	}
+	printf("%d\n", x);
+
+	return (0);
+}
