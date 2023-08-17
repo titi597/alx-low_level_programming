6a9c6e3200b1ca7a4fa2e54db8f8d4418cf9d5af
titi597 <139129034+titi597@users.noreply.github.com>
2023-08-17 15:26:25 -0700

    committed Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,25 +10,29 @@ int main(void)
 {
 	int a;
 
-	for (a = 1 ; a <= 100 ; a++)
+	for (a = 1; a <= 100; a++)
 	{
-	if ((a % 3) == 0 && (a % 5) == 0)
+	if ((a % 3 == 0) && (a % 5 == 0))
+	{
 	printf("FizzBuzz");
-
-	else if ((a % 3) == 0)
+	}
+	else if (a % 3 == 0)
+	{
 	printf("Fizz");
-
-	else if ((a % 5) == 0)
+	}
+	else if (a % 5 == 0)
+	{
 	printf("Buzz");
-
+	}
 	else
+	{
 	printf("%d ", a);
-	if (a == 100)
-	continue;
+	}
+	if (a != 100)
+	{
 	printf(" ");
 	}
-
+	}
 	printf("\n");
-
 	return (0);
 }
