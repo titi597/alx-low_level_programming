5d6529e2f99a93a0cc0677afd69f65fa98cb90e1
titi597 <139129034+titi597@users.noreply.github.com>
2023-08-17 17:58:18 -0700

    Fizz-Buzz committed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - printing number from 1 to 100 with fizz and buzz
+ * main - program that print from 1 to 100
+ *
  * Return: 0 (success)
  */
 
@@ -11,24 +12,23 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-	if ((a % 3 == 0) && (a % 5 == 0))
-	{
-	printf("FizzBuzz ");
-	}
-	else if (a % 3 == 0)
-	{
-	printf("Fizz ");
-	}
-	else if (a % 5 == 0)
-	{
-	printf("Buzz ");
-	}
-	else
-	{
-	printf("%d ", a);
-	}
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+		printf("FizzBuzz ");
+		}
+		else if (a % 3 == 0)
+		{
+		printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+		printf("Buzz ");
+		}
+		else
+		{
+		printf("%d ", a);
+		}
 	}
 	printf("\n");
-
 	return (0);
 }
