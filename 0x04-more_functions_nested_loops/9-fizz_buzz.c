ba3e4cc039919e63a84309285692ef8e4da797db
titi597 <139129034+titi597@users.noreply.github.com>
2023-08-17 18:01:49 -0700

    Fizz_Buzz committed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int a;
 
-	for (a = 1; a <= 100; a++)
+	for (a = 1; a < 100; a++)
 	{
 		if ((a % 3 == 0) && (a % 5 == 0))
 		{
@@ -29,6 +29,7 @@ int main(void)
 		printf("%d ", a);
 		}
 	}
+	printf("Buzz");
 	printf("\n");
 	return (0);
 }
