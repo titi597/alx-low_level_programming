8bf05a5411e38d92b7aa34e4efd3a8fd1e9d7543
titi597 <139129034+titi597@users.noreply.github.com>
2023-08-21 12:44:20 -0700

    committed Arrays are not pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - function that print n elements of an array of integers
+ * @a: array of integers to be used
+ * @n: n elements to be printed
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0 ; b < n ; b++)
+	{
+		printf("%d", a[b]);
+		if (b < n - 1)
+		printf(",");
+		printf(" ");
+	}
+	printf("\n");
+}
