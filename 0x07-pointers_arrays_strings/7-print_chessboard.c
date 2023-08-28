cddf8eb9270bb45efd7e2594d836824deb30a3a8
titi597 <139129034+titi597@users.noreply.github.com>
2023-08-28 06:55:03 -0700

    committed Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_chessboard - a function that prints a chessboard.
@@ -15,8 +16,8 @@ void print_chessboard(char (*a)[8])
 	{
 		for (y = 0; y < 8; y++)
 		{
-			printf("%c ", a[x][y]);
+			_putchar(a[x][y]);
 		}
-		printf("\n");
+		_putchar('\n');
 	}
 }
