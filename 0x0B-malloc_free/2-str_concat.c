b4f7ba5700794285da65112e8f0b9f16538d3c92
titi597 <139129034+titi597@users.noreply.github.com>
2023-09-04 05:51:16 -0700

    committed He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - a function that concatenates two strings.
+ * @s1: string one.
+ * @s2: string two.
+ * Return: pointer should point to a newly allocated space in memory
+ * which contains the contents of s1, followed by the contents of s2.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int a;
+	int b;
+	int c;
+	char *d;
+
+	if ((s1 == NULL) && (s2 == NULL))
+	{
+		s1 = "";
+		s2 = "";
+	}
+	a = 0;
+	for (c = 0; s1[c] != '\0'; c++)
+	{
+		a++;
+	}
+	b = 0;
+	for (c = 0; s2[c] != '\0'; c++)
+	{
+		b++;
+	}
+	d = malloc(sizeof(char) * (a + b) + 1);
+
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	for (c = 0; s1[c] != '\0'; c++)
+	{
+		d[c] = s1[c];
+	}
+	for (c = 0; s2[c] != '\0'; c++)
+	{
+		d[a + c] = s2[c];
+	}
+	return (d);
+
+}
