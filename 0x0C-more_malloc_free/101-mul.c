5788165cc571b8d27606a23629fbb885b4fcb1aa
titi597 <139129034+titi597@users.noreply.github.com>
2023-09-07 16:54:44 -0700

    committed We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,34 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
 /**
- * multiply - multipying two positive numbers
- * @c: first number
- * @d: second number
- * Return: the product of two numbers
+ * _isdigit - fucntion that checks if the string is containing digits
+ * @str: string
+ * Return: 1 is digit and if else non digit 0.
  */
-int multiply(int c, int d)
+int _isdigit(char *str)
 {
-	return (c * d);
+	int p = 0;
+
+	while (str[p])
+	{
+		if (str[p] < '0' || str[p] > '9')
+			return (0);
+		p++;
+	}
+	return (1);
 }
 
 /**
- * isNumber - fucntion that checks if the string is containing digits
+ * _strlen - returing string length
  * @str: string
- * Return: 1
+ * Return: string length
  */
-int isNumber(char *str)
+int _strlen(char *str)
 {
-	while (*str)
+	int p = 0;
+
+	for (; str[p] != '\0'; p++)
 	{
-		if (!isdigit(*str))
-		{
-			return (0);
-		}
-		str++;
 	}
-	return (1);
+	return (p);
+}
+/**
+ * errors - handle the main errors.
+ * Return: void
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
 }
 /**
  * main - a program that multiplies two positive numbers.
@@ -37,28 +49,49 @@ int isNumber(char *str)
  * @argv: quantity of arguments passed
  * Return: 0 (success)
  */
-
 int main(int argc, char *argv[])
 {
-	char *a = argv[1];
-	char *b = argv[2];
-	int c = atoll(a);
-	int d = atoll(b);
-	int result;
+	char *a = NULL, *b = NULL;
+	int i, j, k, l, m, n, o, *result, r = 0;
 
 	if (argc != 3)
+		errors();
+	a = argv[1], b = argv[2];
+	if (!_isdigit(a) || !_isdigit(b))
+		errors();
+	i = _strlen(a), j = _strlen(b);
+	k = i + j;
 	{
-		printf("Error\n");
-		return (98);
+		result = malloc(sizeof(int) * (k + 1));
 	}
-	if (!isNumber(a) || !isNumber(b))
+	if (!result)
+		return (1);
+	for (l = 0; l <= k; l++)
+		result[l] = 0;
+	for (i = i - 1; i >= 0; i--)
 	{
-		printf("Error\n");
-		return (98);
+		n = a[i] - '0';
+		m = 0;
+		for (j = _strlen(b) - 1; j >= 0; j--)
+		{
+			o = b[j] - '0';
+			m += result[i + j + 1] + (n * o);
+			result[i + j + 1] = m % 10;
+			m /= 10;
+		}
+		if (m > 0)
+			result[i + j + 1] += m;
 	}
-	result = multiply(c, d);
-
-	printf("%d\n", result);
-
+	for (l = 0; l < k; l++)
+	{
+		if (result[l])
+			r = 1;
+		if (r)
+			_putchar(result[l] + '0');
+	}
+	if (!r)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
