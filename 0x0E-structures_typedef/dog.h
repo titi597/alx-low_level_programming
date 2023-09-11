d3d8631f400cc9a05b15948608798376d06ca66b
titi597 <139129034+titi597@users.noreply.github.com>
2023-09-11 06:36:18 -0700

    committed Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
