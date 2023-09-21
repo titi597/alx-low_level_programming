d7cda29bc65b45cc4607a016caaf296eac66535a
titi597 <139129034+titi597@users.noreply.github.com>
2023-09-21 14:01:04 -0700

    The Hare and the Tortoise committed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+void comeFirstbyTITI(void) __attribute__((constructor));
+/**
+ * comeFirstbyTITI - function that runs main function
+ *
+ */
+
+void comeFirstbyTITI(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
