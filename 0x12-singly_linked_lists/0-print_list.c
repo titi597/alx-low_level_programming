17c6addf5b32f482bf9e48dd7535b43167297690
titi597 <139129034+titi597@users.noreply.github.com>
2023-09-21 10:22:05 -0700

    Print list committed

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - function that printf all the functions of list_t list
+ * @h: singly linked list
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes;
+
+	nodes = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
