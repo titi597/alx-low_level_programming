08f45113f5aa29cf29ee5c888125cf060b0a1ae1
titi597 <139129034+titi597@users.noreply.github.com>
2023-09-21 13:09:25 -0700

    new updated task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
@@ -22,3 +22,4 @@ size_t print_list(const list_t *h)
 	}
 	return (nodes);
 }
+
