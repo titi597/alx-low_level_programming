ac48e09426eb0da1c8091b39cd68f2a9768c78eb
titi597 <139129034+titi597@users.noreply.github.com>
2023-09-25 13:40:43 -0700

    committed Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -24,23 +24,3 @@ void free_listint2(listint_t **head)
 	}
 	*head = NULL;
 }
-/**
- * create_node - creating new listint_t node
- * @n: new node
- * Return: created new nodes
- */
-listint_t *create_node(int n)
-{
-	listint_t *aptr;
-
-	aptr = (listint_t *)malloc(sizeof(listint_t));
-
-	if (aptr == NULL)
-	{
-		fprintf(stderr, "Memory allocation failed\n");
-		exit(1);
-	}
-	aptr->n = n;
-	aptr->next = NULL;
-	return (aptr);
-}
