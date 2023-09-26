abf1ab25845bbbdde43c6c21e688d3e801c86812
titi597 <139129034+titi597@users.noreply.github.com>
2023-09-26 03:58:00 -0700

    committed Free (safe version)

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - function that frees a listint_t list.
+ * @h: head node
+ * Return: the size of the list that was free’d.
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t aka = 0;
+	listint_t *next;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		aka++;
+		next = (*h)->next;
+		free(*h);
+		*h = next;
+		if (*h)
+		{
+			*h = NULL;
+			break;
+		}
+	}
+	return (aka);
+}
