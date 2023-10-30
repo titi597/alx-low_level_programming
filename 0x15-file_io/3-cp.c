04c66190a237c52932e1075cd68427734618af78
titi597 <139129034+titi597@users.noreply.github.com>
2023-10-30 07:44:46 -0700

    committed cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_TRUNC | O_APPEND, 0664);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_aptr(file_from, file_to, argv);
 
 	tb_read = 1024;
