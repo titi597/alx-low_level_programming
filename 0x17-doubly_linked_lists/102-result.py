a1aceceed21c018aa3814346a1e8ad3b9fec2b8e
titi597 <139129034+titi597@users.noreply.github.com>
2023-12-07 06:43:09 -0800

    Palindromes

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -9,10 +9,6 @@ for i in range(100, 1000):
         product = i * j
         if is_palindrome(product) and product > largest_palindrome:
             largest_palindrome = product
-
-# Print the result to the console
-print(largest_palindrome)
-
 # Save the result to the file
 with open("102-result", "w") as file:
     file.write(str(largest_palindrome))
