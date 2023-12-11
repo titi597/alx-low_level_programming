4f0f4cade06fa4fcc24e9191f15b510b3b3ba26a
titi597 <139129034+titi597@users.noreply.github.com>
2023-12-11 12:32:52 -0800

    updted hacking file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://rawgithubusercontent.com/titi597/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+wget -P .. https://rawgithubusercontent.com/titi597/root/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../libgiga.so"
