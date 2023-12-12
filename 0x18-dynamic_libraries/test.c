4ce9536a1ccb8b459f91f8c8567d63ebf8b0c039
titi597 <139129034+titi597@users.noreply.github.com>
2023-12-11 22:16:56 -0800

    commited changes

--- 0x18-dynamic_libraries/test.c
+++ 0x18-dynamic_libraries/test.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 int printf(const char *format, ...)
 {
-	write(1, "9 8 10 24 75 9\n", 17);
+	write(1, "9 8 10 24 75 +9\n", 17);
 	write(1, "Conglatulations, you win the Jackpot!\n", 38);
 	exit(EXIT_SUCCESS);
 }
