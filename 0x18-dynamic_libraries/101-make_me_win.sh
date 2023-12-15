cb32e66064da96c9a5c9038021c1b7d42287ae7b
titi597 <139129034+titi597@users.noreply.github.com>
2023-12-15 04:01:11 -0800

    hacking almost done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/titi597/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD="$PWD/../libtest.so"
+wget -P .. https://raw.githubusercontent.com/titi597/alx-low_level_programming/master/0x18-dynamic_libraries/libdeng.so
+export LD_PRELOAD="$PWD/../libdeng.so"
