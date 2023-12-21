030388cc42ad014343ea23100783380834a2ab48
titi597 <139129034+titi597@users.noreply.github.com>
2023-12-21 03:48:42 -0800

    4. >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,7 +8,8 @@
  * @ht: The hash table to look into.
  * @key: The key to search for.
  *
- * Return: The value associated with the key, or NULL if the key couldn't be found.
+ * Return: The value associated with the key,
+ * or NULL if the key couldn't be found.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
