b5e2ae6ea5b38af8097bcd3a0b8a15305faa0ac6
titi597 <139129034+titi597@users.noreply.github.com>
2024-02-21 02:43:22 -0800

    5. Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,22 +11,17 @@ def island_perimeter(grid):
     Returns:
     int: The perimeter of the island.
     """
-    perimeter = 0
-    # Iterate through each cell in the grid
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            # If the cell is land, check its perimeter
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                # Check top
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
-                # Check bottom
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
-                # Check left
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
-                # Check right
-                if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
-    return perimeter
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
